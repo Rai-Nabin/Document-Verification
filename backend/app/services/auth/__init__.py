11c9fe9a3cd1ac979245b4d9538480e08b836802
Nabin Rai <kulyangrai10@gmail.com>
2025-03-17 14:12:53 +0545

    __init__.py

--- backend/app/services/auth/__init__.py
+++ backend/app/services/auth/__init__.py
@@ -2,6 +2,7 @@
 File Path: app/services/auth/__init__.py
 """
 
+from .auth_service import auth_service
 from .jwt_handlers import JWTHandler
 
-__all__ = ["JWTHandler"]
+__all__ = ["JWTHandler", "auth_service"]
